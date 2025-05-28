c4d6fdf759c5f37e02f2e23508f86c4dfe08b458
jcorne23 <jcornejo3138@gmail.com>
2025-05-27 20:40:09 -0500

    Update ball (6).sv

--- app/ball (6).sv
+++ app/ball (6).sv
@@ -1,17 +1,3 @@
-//-------------------------------------------------------------------------
-//    Ball.sv                                                            --
-//    Viral Mehta                                                        --
-//    Spring 2005                                                        --
-//                                                                       --
-//    Modified by Stephen Kempf 03-01-2006                               --
-//                              03-12-2007                               --
-//    Translated by Joe Meng    07-07-2013                               --
-//    Fall 2014 Distribution                                             --
-//                                                                       --
-//    For use with ECE 298 Lab 7                                         --
-//    UIUC ECE Department                                                --
-//-------------------------------------------------------------------------
-
 
 module  ball ( input Reset, frame_clk,crash_on, 
                input [4:0]map_on,
